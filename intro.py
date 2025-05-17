b48a00503a7d4cbf61e03b6591b69f84afc8a3ee
Walid Hajeri <hajeriwalid@users.noreply.github.com>
2025-05-17 19:48:25 +0200

    Update intro.py

--- intro.py
+++ intro.py
@@ -13,7 +13,7 @@ cv_data = """
         "location": "Paris Region, France",
         "linkedin": "http://www.linkedin.com/in/walidhajeri"
     },
-    "objective": "Experienced AI / Cloud Solutions Engineer with over 15 years of experience in the tech/cloud industry. Experienced AI / Cloud Solutions Engineer with over 15 years of experience in the tech/cloud industry",
+    "objective": "Experienced AI / Cloud Solutions Engineer with over 15 years of experience in the tech/cloud industry.",
     "experience": [
         {
             "years": "2022-present",
