4051b59b4db574d33c1510a89a497d5fb4d5f91b
Walid Hajeri <hajeriwalid@users.noreply.github.com>
2025-05-19 13:42:27 +0200

    Update intro.py

--- intro.py
+++ intro.py
@@ -233,9 +233,10 @@ def main():
 
     #Anthropic Q&A
     with st.sidebar:
-        anthropic_api_key = st.text_input("Anthropic API Key", key="file_qa_api_key", type="password")
-        "[View the source code](https://github.com/streamlit/llm-examples/blob/main/pages/1_File_Q%26A.py)"
-        "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
+        anthropic_api_key=st.secrets["anthropic_api_key"]
+        #anthropic_api_key = st.text_input("Anthropic API Key", key="file_qa_api_key", type="password")
+        #"[View the source code](https://github.com/streamlit/llm-examples/blob/main/pages/1_File_Q%26A.py)"
+        #"[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
     
     st.title("📝 File Q&A with Anthropic")
     uploaded_file = st.file_uploader("Upload an article", type=("txt", "md"))
